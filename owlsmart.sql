66b38e1d2c294b0e50374ae73c058f6c054aa4bf
gracekalu <empiretown2016@gmail.com>
2020-11-30 00:35:31 -0700

    new styles

--- owlsmart.sql
+++ owlsmart.sql
@@ -8,3 +8,24 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (Username, email, pass) VALUES ('Greg1', 'test@gmail.com', 'Qwerty1');
+
+CREATE TABLE teacher (
+    id SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    pass VARCHAR(50) NOT NULL,
+    schoolname VARCHAR(50),
+    unique(email),
+    FOREIGN KEY (schoolname) REFERENCES school(schoolname)
+
+);
+
+CREATE TABLE school(
+    id SERIAL PRIMARY KEY NOT NULL, 
+    schoolname VARCHAR(100) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    unique(email), CONSTRAINT UX_DERIVED_RELATION UNIQUE (schoolname)
+);
+
+INSERT INTO school (schoolname, email) VALUES ('Holy Trinity Sec School', 'test5@gmail.com');
+INSERT INTO teacher (username, email, pass,schoolname) VALUES ('Jude', 'jude1@gmail.com', 'Qwerty2', 'Holy Trinity Sec School');
