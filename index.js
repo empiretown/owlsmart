3651aad4bc456ce1c7005999f33b92cca730b396
gracekalu <empiretown2016@gmail.com>
2020-12-15 00:24:51 -0700

    new styles

--- index.js
+++ index.js
@@ -386,7 +386,7 @@ app.post('/user/teachersregister', async (req, res) => {
    
 // authenticate the user passport
 app.post('/users/login', passport.authenticate('local', {
-    successRedirect: "/users/dashboard",
+    successRedirect: "/teacher/teacherdashboard",
     failureRedirect: "/users/login",
     session: false,
     failureFlash: true
